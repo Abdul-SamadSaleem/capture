6c159804615cd803a8744dee3289f7a1b55ade38
“Abdul <abdullahplayz07@gmail.com>
2020-11-13 12:34:04 +0000

    Styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 const { default: AboutsUs } = require("./pages/AboutsUs");
+// Global Style
+const {default: GlobalStyle} = require("./components/GlobalStyle")
 
 const App = () => {
     return (
         <div>
+            <GlobalStyle />
             <AboutsUs />
         </div>
     )
