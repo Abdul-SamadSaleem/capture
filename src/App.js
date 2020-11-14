26a5f4552c725d3d83cc2c3c553914e63739b46f
“Abdul <abdullahplayz07@gmail.com>
2020-11-14 15:02:16 +0000

    new changes :)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { Switch, Route, useLocation } from "react-router-dom";
+
+
 const { default: AboutsUs } = require("./pages/AboutsUs");
 // Global Style
 const {default: GlobalStyle} = require("./components/GlobalStyle")
+const {default: Nav} = require("./components/Nav")
+const {default: ContactUs} = require("./pages/ContactUs")
+const {default: OurWork} = require("./pages/OurWork")
+
 
 const App = () => {
     return (
         <div>
             <GlobalStyle />
+            <Nav />
+            <Route path="/" exact>
             <AboutsUs />
+            </Route>
+            <Route path="/work">
+            <OurWork />
+            </Route>
+            <Route path="/contact">        
+            <ContactUs />
+            </Route>
         </div>
     )
 }
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,37 +1,86 @@
+import React from "react";
+import styled from "styled-components";
+import { About } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
+// import { useScroll } from "./useScroll";
+// import { scrollReveal } from "../animation";
+ 
  const FaqSection = () => {
     return (
-        <div className="faq">
-            <h2>Any questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How do I start?</h4>
-                <div className="answer">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum quia sunt in ab tempora aspernatur.
-                </div>
-            </div>
-
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum quia sunt in ab tempora aspernatur.
-                </div>
-            </div>
-
-            <div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum quia sunt in ab tempora aspernatur.
-                </div>
-            </div>
-
-            <div className="question">
-                <h4>What products do you offer?</h4>
-                <div className="answer">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum quia sunt in ab tempora aspernatur.
-                </div>
-            </div>
-
-        </div>
+        <Faq>
+          <h2>
+            Any Questions <span>FAQ</span>
+          </h2>
+          <AnimateSharedLayout>
+            <Toggle title="How Do I Start?">
+              <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+                  autem accusamus ex laboriosam porro, adipisci quam voluptatum
+                  magnam placeat corporis.
+                </p>
+              </div>
+            </Toggle>
+            <Toggle title="Daily Schedule">
+              <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error,
+                  totam.
+                </p>
+              </div>
+            </Toggle>
+            <Toggle title="Diferrent Payment Methods">
+              <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Necessitatibus, neque.
+                </p>
+              </div>
+            </Toggle>
+            <Toggle title="What Products do you offer.">
+              <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Necessitatibus, neque.
+                </p>
+              </div>
+            </Toggle>
+          </AnimateSharedLayout>
+        </Faq>
     )
 }
+
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection
